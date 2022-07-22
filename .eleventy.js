d92cdf70bf38be706721a24c1593f37dddc24ba5
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2022-07-22 14:48:08 +0200

    add debugger

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,23 @@
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
-    eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/assets'); 
-    eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addFilter("debugger", (...args) => {
+    console.log(...args);
+    debugger;
+  });
 
-    eleventyConfig.addFilter("postDate", (dateObj) =>{
-      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    });
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
-    return {
-        dir: {
-          input: "src",
-          output: "public"
-        }
-      }
-}
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
